ab1dc459833f6752c325bd34b32415e522d87be2
Swetabh Shreyam <SwetabhS@ZC-SwetabhS.local>
2025-03-10 15:57:02 +0530

    updated validation upper_limit

--- app/services/validation_service.rb
+++ app/services/validation_service.rb
@@ -4,11 +4,9 @@ class ValidationService
 
     allowed_modes = %w[arcade classic solo ranked team]
     errors = Array.new
-
-
-
+    
     errors << "Missing parameteres" unless params[:user_id].present? && params[:score].present? && params[:game_mode].present?
-    errors << "Score must be a positive integer" unless params[:score].to_i.positive?
+    errors << "Score must be a positive integer less than equal to 1000" unless params[:score].to_i.positive? && params[:score].to_i <= 1000
     errors <<  "Invalid game_mode" unless allowed_modes.include?(params[:game_mode])
 
     errors.empty? ? nil:errors
